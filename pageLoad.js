426b9092e7e9069a8b5528893962e28aa48dd542
arpad <arpadbotond.rigo@outlook.com>
2024-01-25 13:48:45 -0500

    update competition page v1.0

--- pageLoad.js
+++ pageLoad.js
@@ -5,7 +5,7 @@ window.onload = function(){
 
 function homePageLoad(){
     if(navigator.userAgent.includes("Android") || navigator.userAgent.includes("iPhone")){
-        document.getElementById('text').innerHTML="Currently studying in Computer Science at University of Montreal and studied pre-universitary Computer Science and Mathematics at Bois-de-Boulogne College. I'm mostly interested in cybersecurity and software development in diverse programming languages such as C, C++ and Java. Former teacher assistant in Object-Oriented Programming course at my university, I also worked as a project manager to implement a temporary data base as a student job.";
+        document.getElementById('text-intro').innerHTML="Currently studying in Computer Science at University of Montreal and studied pre-universitary Computer Science and Mathematics at Bois-de-Boulogne College. I'm mostly interested in cybersecurity and software development in diverse programming languages such as C, C++ and Java. Former teacher assistant in Object-Oriented Programming course at my university, I also worked as a project manager to implement a temporary data base as a student job.";
         document.getElementById('nav-bar').style.height="100px"
         document.getElementById('home-link').style.fontSize="40px";
         document.getElementById('comp-link').style.fontSize="40px";
@@ -13,7 +13,7 @@ function homePageLoad(){
         document.getElementById('project-link').style.fontSize="40px";
     
       } else {
-                var typed = new Typed('#text', {
+                var typed = new Typed('#text-intro', {
                 strings : ["Currently studying in Computer Science at University of Montreal and studied pre-universitary Computer Science and Mathematics at Bois-de-Boulogne College. I'm mostly interested in cybersecurity and software development in diverse programming languages such as C, C++ and Java. Former teacher assistant in Object-Oriented Programming course at my university, I also worked as a project manager to implement a temporary data base as a student job."],
                 typeSpeed:20,
                 loop: false
