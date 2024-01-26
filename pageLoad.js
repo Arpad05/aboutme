3c9116b7efe7bd4fd210e0c96decf313f7459926
Arpad <arpadbotond.rigo@outlook.com>
2024-01-25 23:50:27 -0500

    competition page v1.02

--- pageLoad.js
+++ pageLoad.js
@@ -1,8 +1,6 @@
 window.onload = function(){
     homePageLoad();
 }
-
-
 function homePageLoad(){
     if(navigator.userAgent.includes("Android") || navigator.userAgent.includes("iPhone")){
         document.getElementById('text-intro').innerHTML="Currently studying in Computer Science at University of Montreal and studied pre-universitary Computer Science and Mathematics at Bois-de-Boulogne College. I'm mostly interested in cybersecurity and software development in diverse programming languages such as C, C++ and Java. Former teacher assistant in Object-Oriented Programming course at my university, I also worked as a project manager to implement a temporary data base as a student job.";
@@ -12,10 +10,10 @@ function homePageLoad(){
         document.getElementById('occ-link').style.fontSize="2.5vh";
         document.getElementById('project-link').style.fontSize="2.5vh";
         document.getElementById("navigation").style.padding="5%";
-    
       } else {
-        document.getElementsByTagName('nav').style.padding="15px";
-        var typed = new Typed('#text-intro', {
+        console.log("problem")
+        document.getElementById("navigation").style.padding="15px";
+          var typed = new Typed('#text-intro', {
             strings : ["Currently studying in Computer Science at University of Montreal and studied pre-universitary Computer Science and Mathematics at Bois-de-Boulogne College. I'm mostly interested in cybersecurity and software development in diverse programming languages such as C, C++ and Java. Former teacher assistant in Object-Oriented Programming course at my university, I also worked as a project manager to implement a temporary data base as a student job."],
             typeSpeed:20,
             loop: false
