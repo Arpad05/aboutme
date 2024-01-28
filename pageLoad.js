19fac8e7a08d502d6a2bda9cfc31177b6d513bcc
Arpad <arpadbotond.rigo@outlook.com>
2024-01-27 20:34:45 -0500

    css update

--- pageLoad.js
+++ pageLoad.js
@@ -14,8 +14,8 @@ function homePageLoad(){
         console.log("problem")
         document.getElementById("navigation").style.padding="15px";
           var typed = new Typed('#text-intro', {
-            strings : ["Currently studying in Computer Science at University of Montreal and studied pre-universitary Computer Science and Mathematics at Bois-de-Boulogne College. I'm mostly interested in cybersecurity and software development in diverse programming languages such as C, C++ and Java. Former teacher assistant in Object-Oriented Programming course at my university, I also worked as a project manager to implement a temporary data base as a student job."],
-            typeSpeed:20,
+            strings : ["Currently studying in Computer Science at University of Montreal and studied pre-universitary Computer Science and Mathematics at Bois-de-Boulogne College, I'm mostly interested in cybersecurity and software development in diverse programming languages such as C, C++ and Java. Former teacher assistant in Object-Oriented Programming course at my university, I also worked as a project manager to implement a temporary data base as a student job."],
+            typeSpeed:12,
             loop: false
       });
     }
